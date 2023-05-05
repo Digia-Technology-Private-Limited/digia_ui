d49d1b1338257a3d0a1c2193b2e388fd1b50dccb
Tushar Gupta <tushar.at.dce@gmail.com>
2023-05-05 18:03:23 +0530

    removed unused imports

--- lib/Utils/util_functions.dart
+++ lib/Utils/util_functions.dart
@@ -1,6 +1,5 @@
 import 'package:digia_ui/Utils/dui_font.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class DUIConfigConstants {
   static double fallbackSize = 14;
