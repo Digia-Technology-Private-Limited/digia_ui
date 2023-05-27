65a37ac671b7d931f43c1cb6457a146b134d8062
Tushar Gupta <tushar.at.dce@gmail.com>
2023-05-27 20:57:20 +0530

    removed mocked value

--- lib/components/techCard/tech_card.props.dart
+++ lib/components/techCard/tech_card.props.dart
@@ -5,7 +5,6 @@ import 'package:digia_ui/components/image/image.props.dart';
 import 'package:digia_ui/components/utils/DUIStyleClass/dui_style_class.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-
 part 'tech_card.props.g.dart';
 
 @JsonSerializable()
@@ -28,43 +27,5 @@ class DUITechCardProps {
   @override
   String toString() {
     return jsonEncode(this);
-  }
-
-  static DUITechCardProps mockWidget() {
-    return DUITechCardProps.fromJson({
-      "height": 160,
-      "width": 200,
-      "image": {
-        "height": 48,
-        "width": 48,
-        "aspectRatio": 1,
-        "imageSrc":
-            "https://upload.wikimedia.org/wikipedia/commons/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg",
-        "fit": "fill",
-        "cornerRadius": {
-          "topRight": 25,
-          "topLeft": 25,
-          "bottomLeft": 25,
-          "bottomRight": 25,
-        }
-      },
-      "bgColor": "accent1",
-      "title": {
-        "styleClass": "ft:h2-medium;tc:text",
-        "textSpans": [
-          {
-            "text": "Kotlin",
-          },
-        ]
-      },
-      "subText": {
-        "styleClass": "ft:para3;tc:textSubtle",
-        "textSpans": [
-          {
-            "text": "Programming",
-          },
-        ]
-      }
-    });
   }
 }
