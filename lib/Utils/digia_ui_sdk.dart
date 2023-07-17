bcdb50c8e1edb3b7c96b33e1a0bab368772b50c1
anupamsingh-digia <digia0211>
2023-07-17 17:44:07 +0530

    renamed initalizer

--- lib/Utils/digia_ui_sdk.dart
+++ lib/Utils/digia_ui_sdk.dart
@@ -3,8 +3,8 @@
 import '../core/pref/pref_util.dart';
 import 'config_resolver.dart';
 
-class DigiaUiSDkInitializer {
-  static Future<void> initialize(String assetPath) async {
+class DigiaUiSDk{
+  static  initialize(String assetPath) async {
     // Perform SDK initialization tasks here
     // This could include setting up configurations, initializing services, etc.
     // Load configuration
