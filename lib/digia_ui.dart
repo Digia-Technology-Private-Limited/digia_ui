3d4efc4c3996c8ab475d79750958b7774d73513f
Aditya Choubey <choubeyaditya80@gmail.com>
2024-02-12 12:44:49 +0530

    Export dui_widget_json_data.dart

--- lib/digia_ui.dart
+++ lib/digia_ui.dart
@@ -4,3 +4,4 @@ export 'package:digia_ui/src/digia_ui_provider.dart';
 export 'package:digia_ui/src/digia_ui_sdk.dart';
 export 'package:digia_ui/src/core/page/dui_page.dart';
 export 'package:digia_ui/src/components/dui_widget.dart';
+export 'package:digia_ui/src/core/page/props/dui_widget_json_data.dart';
