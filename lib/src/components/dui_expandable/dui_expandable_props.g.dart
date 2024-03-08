7b0571f2ca21e58f0a51a418c0e60441b4d99a80
amar <amarrawat244@gmail.com>
2024-03-08 16:27:05 +0530

    ran build runner command

--- lib/src/components/dui_expandable/dui_expandable_props.g.dart
+++ lib/src/components/dui_expandable/dui_expandable_props.g.dart
@@ -8,10 +8,48 @@ part of 'dui_expandable_props.dart';
 
 DUIExpandableProps _$DUIExpandablePropsFromJson(Map<String, dynamic> json) =>
     DUIExpandableProps(
-      json['title'] as String?,
+      json['expanded'] as Map<String, dynamic>?,
+      json['collapsed'] as Map<String, dynamic>?,
+      json['bodyAlignment'] as String?,
+      json['headerAlignment'] as String?,
+      json['iconPlacement'] as String?,
+      json['color'] as String?,
+      json['padding'] == null ? null : DUIInsets.fromJson(json['padding']),
+      json['alignment'] as String?,
+      json['animationDuration'] as int?,
+      json['collapseIcon'] as Map<String, dynamic>?,
+      json['expandIcon'] as Map<String, dynamic>?,
+      (json['iconSize'] as num?)?.toDouble(),
+      (json['iconRotationAngle'] as num?)?.toDouble(),
+      json['hasIcon'] as bool?,
+      json['tapHeaderToExpand'] as bool?,
+      json['tapBodyToExpand'] as bool?,
+      json['tapBodyToCollapse'] as bool?,
+      json['useInkWell'] as bool?,
+      json['inkWellBorderRadius'] == null
+          ? null
+          : DUIBorder.fromJson(json['inkWellBorderRadius']),
     );
 
 Map<String, dynamic> _$DUIExpandablePropsToJson(DUIExpandableProps instance) =>
     <String, dynamic>{
-      'title': instance.title,
+      'expanded': instance.expanded,
+      'collapsed': instance.collapsed,
+      'bodyAlignment': instance.bodyAlignment,
+      'headerAlignment': instance.headerAlignment,
+      'iconPlacement': instance.iconPlacement,
+      'color': instance.color,
+      'alignment': instance.alignment,
+      'padding': instance.padding,
+      'animationDuration': instance.animationDuration,
+      'collapseIcon': instance.collapseIcon,
+      'expandIcon': instance.expandIcon,
+      'iconSize': instance.iconSize,
+      'iconRotationAngle': instance.iconRotationAngle,
+      'hasIcon': instance.hasIcon,
+      'tapHeaderToExpand': instance.tapHeaderToExpand,
+      'tapBodyToExpand': instance.tapBodyToExpand,
+      'tapBodyToCollapse': instance.tapBodyToCollapse,
+      'useInkWell': instance.useInkWell,
+      'inkWellBorderRadius': instance.inkWellBorderRadius,
     };
