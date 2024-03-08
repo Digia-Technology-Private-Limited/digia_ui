0ab0102b9ad40b6fa27f4ad9388d5361b41bad11
amar <amarrawat244@gmail.com>
2024-03-08 14:59:23 +0530

    added expandable_props.g file

---
+++ lib/src/components/dui_expandable/dui_expandable_props.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dui_expandable_props.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DUIExpandableProps _$DUIExpandablePropsFromJson(Map<String, dynamic> json) =>
+    DUIExpandableProps(
+      json['title'] as String?,
+    );
+
+Map<String, dynamic> _$DUIExpandablePropsToJson(DUIExpandableProps instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+    };
