6b8a9536115ed7fbe03d832fc9f1c38e8774c5ca
Premansh Tomar <premanshtomar@gmail.com>
2024-04-10 16:04:43 +0530

    created generated file

--- lib/src/components/dui_icon_button/dui_icon_button_props.g.dart
+++ lib/src/components/dui_icon_button/dui_icon_button_props.g.dart
@@ -1,1 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
 part of 'dui_icon_button_props.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DUIIconButtonProps _$DUIIconButtonPropsFromJson(Map<String, dynamic> json) =>
+    DUIIconButtonProps(
+      json['color'] as String?,
+    );
+
+Map<String, dynamic> _$DUIIconButtonPropsToJson(DUIIconButtonProps instance) =>
+    <String, dynamic>{
+      'color': instance.color,
+    };
