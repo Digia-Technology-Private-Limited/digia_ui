71c898e6eb16aeeab3c2f8b7ff8d80baa3370e0e
Anurag_Singh <singh.anurag04122@gmail.com>
2024-04-11 16:02:09 +0530

    added props file

--- lib/src/components/dui_icon_button/dui_icon_button_props.dart
+++ lib/src/components/dui_icon_button/dui_icon_button_props.dart
@@ -1,15 +1,39 @@
+import 'package:digia_ui/src/components/dui_icons/dui_icon_props.dart';
+import 'package:digia_ui/src/components/utils/DUIInsets/dui_insets.dart';
+import 'package:digia_ui/src/core/action/action_prop.dart';
+import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'dui_icon_button_props.g.dart';
 
 @JsonSerializable()
 class DUIIconButtonProps {
- final String? color;
+  final DUIIconProps? icon;
+  final String? iconColor;
+  final double? iconSize;
+  final DUIInsets? padding;
+  final String? alignment;
+  final ButtonStyle? style;
+  final ActionProp? onClick;
+  final String? childAlignment;
+  final double? elevation;
+  final String? backgroundColor;
 
-  DUIIconButtonProps(this.color);
+  DUIIconButtonProps({
+      this.icon,
+      this.onClick,
+      this.iconColor,
+      this.iconSize,
+      this.padding,
+      this.alignment,
+      this.style,
+      this.childAlignment,
+      this.backgroundColor,
+      this.elevation,
+    });
 
-//   factory DUIIconButtonProps.fromJson(Map <String, dynamic> json)=>;
+  factory DUIIconButtonProps.fromJson(Map<String, dynamic> json) =>
+      _$DUIIconButtonPropsFromJson(json);
 
-// Map<String , dynamic> toJson() =>;
-
+  Map<String, dynamic> toJson() => _$DUIIconButtonPropsToJson(this);
 }
