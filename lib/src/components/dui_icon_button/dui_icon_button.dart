88e197fc3d6f93bccc68666ea26dfeb94ff73a8a
Anurag_Singh <singh.anurag04122@gmail.com>
2024-04-11 15:28:01 +0530

    updated icon button files

--- lib/src/components/dui_icon_button/dui_icon_button.dart
+++ lib/src/components/dui_icon_button/dui_icon_button.dart
@@ -1,5 +1,14 @@
+import 'package:digia_ui/src/Utils/basic_shared_utils/dui_decoder.dart';
+import 'package:digia_ui/src/Utils/basic_shared_utils/lodash.dart';
+import 'package:digia_ui/src/Utils/util_functions.dart';
 import 'package:digia_ui/src/components/dui_icon_button/dui_icon_button_props.dart';
+import 'package:digia_ui/src/components/dui_icons/dui_icon.dart';
+import 'package:digia_ui/src/components/dui_icons/dui_icon_props.dart';
+import 'package:digia_ui/src/core/page/dui_page_event.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/page/dui_page_bloc.dart';
 
 class DUIIconButton extends StatefulWidget {
   final DUIIconButtonProps props;
@@ -12,6 +21,23 @@ class DUIIconButton extends StatefulWidget {
 class _DUIIconButtonState extends State<DUIIconButton> {
   @override
   Widget build(BuildContext context) {
-    return IconButton(onPressed: (){}, icon: const Icon(Icons.abc), );
+    final bloc = context.read<DUIPageBloc>();
+    return IconButton(
+      icon: DUIIcon(DUIIconProps.fromJson(widget.props.icon)),
+      onPressed: () async {
+        bloc.add(
+            PostActionEvent(action: widget.props.onClick!, context: context));
+      },
+      color: widget.props.iconColor.letIfTrue(toColor),
+      iconSize: widget.props.iconSize,
+      padding: DUIDecoder.toEdgeInsets(widget.props.padding?.toJson()),
+      alignment: DUIDecoder.toAlignment(widget.props.alignment),
+      style: ButtonStyle(
+        alignment: DUIDecoder.toAlignment(widget.props.childAlignment),
+        backgroundColor: MaterialStateColor.resolveWith((states) =>
+            widget.props.backgroundColor.letIfTrue(toColor) ?? Colors.blueAccent),
+        elevation: MaterialStatePropertyAll(widget.props.elevation),
+      ),
+    );
   }
 }
--- lib/src/core/builders/dui_icon_button_builder.dart
+++ lib/src/core/builders/dui_icon_button_builder.dart
@@ -1,4 +1,5 @@
 import 'package:digia_ui/src/components/dui_icon_button/dui_icon_button.dart';
+import 'package:digia_ui/src/components/dui_icon_button/dui_icon_button_props.dart';
 import 'package:digia_ui/src/core/json_widget_builder.dart';
 import 'package:digia_ui/src/core/page/props/dui_widget_json_data.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,6 @@ class DUIIconButtonBuilder extends DUIWidgetBuilder{
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DUIIconButton(props:DUIIconButtonProps.fromJson(data.props),);
   }
 }
