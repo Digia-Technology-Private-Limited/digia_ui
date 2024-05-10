0b07f8987daf75eb2db265aa49f8d8a15a09142c
Premansh Tomar <premanshtomar@gmail.com>
2024-05-10 14:11:32 +0530

    made a custom widget and tested

--- lib/src/core/builders/dui_custom_widget.dart
+++ lib/src/core/builders/dui_custom_widget.dart
@@ -1,3 +1,4 @@
+import 'package:digia_ui/src/Utils/basic_shared_utils/color_decoder.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -90,7 +91,7 @@ class _DUICustomWidgetState extends State<DUICustomWidget> {
             children: List.generate(
               (widget.json['toggleItems'][tagIndex]['items'] as List<dynamic>)
                   .length,
-              (index) => InkWell(
+                  (index) => InkWell(
                 onTap: () {
                   setItemIndex(index);
                 },
@@ -103,7 +104,7 @@ class _DUICustomWidgetState extends State<DUICustomWidget> {
                   ),
                   child: Text(
                     (widget.json['toggleItems'][tagIndex]['items'][index]
-                        as Map<String, dynamic>)['timeframe'],
+                    as Map<String, dynamic>)['timeframe'],
                     style: GoogleFonts.inter().copyWith(
                       fontSize: 14,
                       fontWeight: FontWeight.w400,
@@ -113,6 +114,9 @@ class _DUICustomWidgetState extends State<DUICustomWidget> {
                 ),
               ),
             ),
+          ),
+          const SizedBox(
+            height: 32,
           ),
           Text(
             (widget.json['toggleItems'][tagIndex]['items'][itemIndex]
@@ -125,13 +129,53 @@ class _DUICustomWidgetState extends State<DUICustomWidget> {
           ),
           Row(
             children: [
-              Text(
-                (widget.json['toggleItems'][tagIndex]['items'][itemIndex]
-                    as Map<String, dynamic>)['graphData'][0]['valueText'],
-                style: GoogleFonts.inter().copyWith(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFFE7E6E2),
+              Expanded(
+                flex: 3,
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 8, bottom: 12),
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: ColorDecoder.fromHexString(
+                                  (widget.json['toggleItems'][tagIndex]['items']
+                                              [itemIndex]
+                                          as Map<String, dynamic>)['graphData']
+                                      [0]['color'])
+                              .withOpacity(0)),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(right: 8, bottom: 12),
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: ColorDecoder.fromHexString(
+                              (widget.json['toggleItems'][tagIndex]
+                                              ['items']
+                                          [itemIndex]
+                                      as Map<String, dynamic>)['graphData'][0]
+                                  ['valueColor'])),
+                      width: MediaQuery.sizeOf(context).width *
+                          (((widget.json['toggleItems'][tagIndex]['items']
+                                              [itemIndex]
+                                          as Map<String, dynamic>)['graphData']
+                                      [0]['barWidth'] /
+                                  ((tagIndex == 0) ? 1000000 : 1)) /
+                              100),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Text(
+                  '+ ${(widget.json['toggleItems'][tagIndex]['items'][itemIndex] as Map<String, dynamic>)['graphData'][0]['valueText']}',
+                  style: GoogleFonts.inter().copyWith(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFFE7E6E2),
+                  ),
                 ),
               )
             ],
@@ -145,15 +189,57 @@ class _DUICustomWidgetState extends State<DUICustomWidget> {
               color: const Color(0xFFBBB9B7),
             ),
           ),
+          const SizedBox(
+            height: 24,
+          ),
           Row(
             children: [
-              Text(
-                (widget.json['toggleItems'][tagIndex]['items'][itemIndex]
-                    as Map<String, dynamic>)['graphData'][1]['valueText'],
-                style: GoogleFonts.inter().copyWith(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFFE7E6E2),
+              Expanded(
+                flex: 3,
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(right: 8, bottom: 12),
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: ColorDecoder.fromHexString(
+                                  (widget.json['toggleItems'][tagIndex]['items']
+                                              [itemIndex]
+                                          as Map<String, dynamic>)['graphData']
+                                      [1]['color'])
+                              .withOpacity(0),
+                          borderRadius: BorderRadius.circular(4)),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(right: 8, bottom: 12),
+                      height: 50,
+                      width: MediaQuery.sizeOf(context).width *
+                          (((widget.json['toggleItems'][tagIndex]['items']
+                                              [itemIndex]
+                                          as Map<String, dynamic>)['graphData']
+                                      [1]['barWidth'] /
+                                  ((tagIndex == 0) ? 1000000 : 1)) /
+                              100),
+                      decoration: BoxDecoration(
+                          color: ColorDecoder.fromHexString(
+                              (widget.json['toggleItems'][tagIndex]
+                                              ['items']
+                                          [itemIndex]
+                                      as Map<String, dynamic>)['graphData'][1]
+                                  ['valueColor']),
+                          borderRadius: BorderRadius.circular(4)),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  '+ ${(widget.json['toggleItems'][tagIndex]['items'][itemIndex] as Map<String, dynamic>)['graphData'][1]['valueText']}',
+                  style: GoogleFonts.inter().copyWith(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: const Color(0xFFE7E6E2),
+                  ),
                 ),
               )
             ],
@@ -162,29 +248,34 @@ class _DUICustomWidgetState extends State<DUICustomWidget> {
             padding: const EdgeInsets.all(12),
             margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
             decoration: BoxDecoration(
-                border: Border.all(
-                    color: const Color(0xffffffff1a).withOpacity(0.1)),
+                border:
+                    Border.all(color: const Color(0xffFFFFFF).withOpacity(0.3)),
                 borderRadius: BorderRadius.circular(12)),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.check_circle,
-                      color: Color(0xFF4EB3A9),
-                    ),
-                    Text(
-                      ((widget.json['toggleItems'][tagIndex]['items'][itemIndex]
-                              as Map<String, dynamic>)['note']
-                          as Map<String, dynamic>)['title'],
-                      style: GoogleFonts.inter().copyWith(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: const Color(0xFFE7E6E2),
+                FittedBox(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.check_circle,
+                        color: Color(0xFF4EB3A9),
                       ),
-                    )
-                  ],
+                      Text(
+                        ((widget.json['toggleItems'][tagIndex]['items']
+                                [itemIndex] as Map<String, dynamic>)['note']
+                            as Map<String, dynamic>)['title'],
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.inter().copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: const Color(0xFFE7E6E2),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 const SizedBox(
                   height: 4,
@@ -193,7 +284,9 @@ class _DUICustomWidgetState extends State<DUICustomWidget> {
                   ((widget.json['toggleItems'][tagIndex]['items'][itemIndex]
                           as Map<String, dynamic>)['note']
                       as Map<String, dynamic>)['subtitle'],
+                  textAlign: TextAlign.center,
                   style: GoogleFonts.inter().copyWith(
+                    height: 1.5,
                     fontSize: 14,
                     fontWeight: FontWeight.w400,
                     color: const Color(0xFFBBB9B7),
