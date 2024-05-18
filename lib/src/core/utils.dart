f22aef7bb4e96a9a0f9341f92de91b61e788ecec
Premansh Tomar <premanshtomar@gmail.com>
2024-05-18 18:25:09 +0530

    refactored some code

--- lib/src/core/utils.dart
+++ lib/src/core/utils.dart
@@ -1,17 +1,17 @@
-import 'package:digia_ui/src/core/builders/dui_icon_builder.dart';
-import 'package:digia_ui/src/core/builders/dui_text_builder.dart';
 import 'package:flutter/material.dart';
 
 import '../Utils/basic_shared_utils/dui_decoder.dart';
 import '../Utils/basic_shared_utils/lodash.dart';
 import '../Utils/util_functions.dart';
+import 'builders/dui_icon_builder.dart';
+import 'builders/dui_text_builder.dart';
 import 'evaluator.dart';
 import 'page/dui_page.dart';
 
 Future<Object?> openDUIPage(
     {required String pageUid,
     required BuildContext context,
-  Map<String, dynamic>? pageArgs}) {
+    Map<String, dynamic>? pageArgs}) {
   return Navigator.push(
     context,
     MaterialPageRoute(
@@ -33,7 +33,7 @@ Future<Object?> openDUIPageInBottomSheet({
 }) {
   return showModalBottomSheet(
     barrierColor: eval<String>(style['bgColor']['data'], context: context)
-        ?.letIfTrue(toColor) ??
+            ?.letIfTrue(toColor) ??
         Colors.black.withOpacity(0.4),
     context: context,
     builder: (ctx) {
@@ -62,11 +62,11 @@ Future<Object?> openDUIPageInBottomSheet({
                   ? Border.all(
                       style: BorderStyle.solid,
                       color: eval<String>(style['borderColor']['data'],
-                    context: context)
-                    ?.letIfTrue(toColor) ??
-                    Colors.transparent,
-                width: (style['borderWidth']['data'] as double?) ?? 1.0,
-              )
+                                  context: context)
+                              ?.letIfTrue(toColor) ??
+                          Colors.transparent,
+                      width: (style['borderWidth']['data'] as double?) ?? 1.0,
+                    )
                   : null,
             ),
             child: DUIPage(
