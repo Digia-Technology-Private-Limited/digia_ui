51b6d793239eadd959a87572bce646e8fc7bd18a
Premansh Tomar <premanshtomar@gmail.com>
2024-05-19 12:07:12 +0530

    minor UI changes in open page in bottomsheet

--- lib/src/core/utils.dart
+++ lib/src/core/utils.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../Utils/basic_shared_utils/dui_decoder.dart';
 import '../Utils/basic_shared_utils/lodash.dart';
 import '../Utils/basic_shared_utils/num_decoder.dart';
@@ -26,7 +25,7 @@ Future<Object?> openDUIPage(
   );
 }
 
-Future<Object?> openDUIPageInBottomSheet({
+Future<Widget?> openDUIPageInBottomSheet({
   required String pageUid,
   required BuildContext context,
   required Map<String, dynamic> style,
@@ -38,41 +37,57 @@ Future<Object?> openDUIPageInBottomSheet({
             Colors.black.withOpacity(0.4),
     context: context,
     builder: (ctx) {
-      return Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                DUITextBuilder.fromProps(props: style['text']).build(context),
-                DUIIconBuilder.fromProps(props: style['icon']).build(context)
-              ],
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: DUIDecoder.toBorderRadius(style['borderRadius']),
+          border: (style['borderStyle'] != null &&
+                  style['borderStyle'] == 'solid')
+              ? Border.all(
+                  style: BorderStyle.solid,
+                  color: eval<String>(style['borderColor'], context: context)
+                          .letIfTrue(toColor) ??
+                      Colors.transparent,
+                  width: NumDecoder.toDoubleOrDefault(style['borderWidth'],
+                      defaultValue: 1.0),
+                )
+              : null,
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                height: MediaQuery.sizeOf(context).height * 0.04,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (style['text'] != null)
+                      FittedBox(
+                          child: DUITextBuilder.fromProps(props: style['text'])
+                              .build(context)),
+                    if (style['icon'] != null)
+                      FittedBox(
+                        child: InkWell(
+                            onTap: () {
+                              Navigator.pop(ctx);
+                            },
+                            child:
+                                DUIIconBuilder.fromProps(props: style['icon'])
+                                    .build(context)),
+                      )
+                  ],
+                ),
+              ),
             ),
-          ),
-          Container(
-            height: MediaQuery.sizeOf(context).height * 0.6,
-            decoration: BoxDecoration(
-              borderRadius: DUIDecoder.toBorderRadius(style['borderRadius']),
-              border: (style['borderStyle'] != null &&
-                      style['borderStyle'] == 'solid')
-                  ? Border.all(
-                      style: BorderStyle.solid,
-                      color:
-                          eval<String>(style['borderColor'], context: context)
-                                  .letIfTrue(toColor) ??
-                              Colors.transparent,
-                      width: NumDecoder.toDoubleOrDefault(style['borderWidth'],
-                          defaultValue: 1.0),
-                    )
-                  : null,
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.52,
+              child: DUIPage(
+                pageUid: pageUid,
+                pageArgs: pageArgs,
+              ),
             ),
-            child: DUIPage(
-              pageUid: pageUid,
-              pageArgs: pageArgs,
-            ),
-          ),
-        ],
+          ],
+        ),
       );
     },
   );
