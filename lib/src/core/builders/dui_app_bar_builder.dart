93f4f00e06f4aa6d6ab87c9df319cef808b481c7
Tushar Gupta <tushar.at.dce@gmail.com>
2024-05-23 01:56:56 +0530

    bug fix - elevation of appbar

--- lib/src/core/builders/dui_app_bar_builder.dart
+++ lib/src/core/builders/dui_app_bar_builder.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../Utils/basic_shared_utils/lodash.dart';
 import '../../Utils/util_functions.dart';
+import '../evaluator.dart';
 import '../json_widget_builder.dart';
 import '../page/props/dui_widget_json_data.dart';
 import 'dui_text_builder.dart';
@@ -18,7 +19,7 @@ class DUIAppBarBuilder extends DUIWidgetBuilder {
     return AppBar(
         title:
             DUITextBuilder.fromProps(props: data.props['title']).build(context),
-        elevation: data.props['elevation'],
+        elevation: eval<double>(data.props['elevation'], context: context),
         shadowColor: (data.props['shadowColor'] as String?).letIfTrue(toColor),
         backgroundColor:
             (data.props['backgrounColor'] as String?).letIfTrue(toColor),
