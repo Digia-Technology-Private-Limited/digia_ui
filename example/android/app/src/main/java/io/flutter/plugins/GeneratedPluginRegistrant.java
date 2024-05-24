64c02847763718d5a2fe019c9b516de39a0c3591
anupamsingh-digia <digia0211>
2024-05-24 16:45:02 +0530

    ## [0.0.4] - 2023-05-23

--- example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -16,11 +16,6 @@ public final class GeneratedPluginRegistrant {
   private static final String TAG = "GeneratedPluginRegistrant";
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
     try {
-      flutterEngine.getPlugins().add(new io.flutter.plugins.deviceinfo.DeviceInfoPlugin());
-    } catch (Exception e) {
-      Log.e(TAG, "Error registering plugin device_info, io.flutter.plugins.deviceinfo.DeviceInfoPlugin", e);
-    }
-    try {
       flutterEngine.getPlugins().add(new com.mixpanel.mixpanel_flutter.MixpanelFlutterPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin mixpanel_flutter, com.mixpanel.mixpanel_flutter.MixpanelFlutterPlugin", e);
@@ -34,11 +29,6 @@ public final class GeneratedPluginRegistrant {
       flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin path_provider_android, io.flutter.plugins.pathprovider.PathProviderPlugin", e);
-    }
-    try {
-      flutterEngine.getPlugins().add(new com.di1shuai.platform_device_id.PlatformDeviceIdPlugin());
-    } catch (Exception e) {
-      Log.e(TAG, "Error registering plugin platform_device_id_v3, com.di1shuai.platform_device_id.PlatformDeviceIdPlugin", e);
     }
     try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
