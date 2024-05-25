6ca162c48f3a0c04f872fcead7c06177eb9269c4
amar <amarrawat244@gmail.com>
2024-05-25 19:06:44 +0530

    added some properties and fixed logic in dial pad

--- lib/src/core/builders/dezerv_component/dezerv_dial_pad_builder.dart
+++ lib/src/core/builders/dezerv_component/dezerv_dial_pad_builder.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 import '../../json_widget_builder.dart';
 import '../../page/props/dui_widget_json_data.dart';
 import 'dezerv_dial_pad_widget.dart';
+import 'dezerv_dial_pad_widget_props.dart';
 
 class DUIDezervDialPadBuilder extends DUIWidgetBuilder {
-  DUIDezervDialPadBuilder({required super.data});
+  DUIDezervDialPadBuilder(DUIWidgetJsonData data) : super(data: data);
 
   static DUIDezervDialPadBuilder? create(DUIWidgetJsonData data) {
-    return DUIDezervDialPadBuilder(data: data);
+    return DUIDezervDialPadBuilder(data);
   }
 
   @override
   Widget build(BuildContext context) {
-    return const DezervDialPad();
+    return DezervDialPad(
+      props: DezervDialPadProps.fromJson(data.props),
+    );
   }
 }
---
+++ lib/src/core/builders/dezerv_component/dezerv_dial_pad_widget_props.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dezerv_dial_pad_widget_props.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DezervDialPadProps _$DezervDialPadPropsFromJson(Map<String, dynamic> json) =>
+    DezervDialPadProps(
+      defaultAmount: (json['defaultAmount'] as num?)?.toDouble(),
+      maximumSipAmount: (json['maximumSipAmount'] as num?)?.toDouble(),
+      minimumSipAmount: (json['minimumSipAmount'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$DezervDialPadPropsToJson(DezervDialPadProps instance) =>
+    <String, dynamic>{
+      'defaultAmount': instance.defaultAmount,
+      'maximumSipAmount': instance.maximumSipAmount,
+      'minimumSipAmount': instance.minimumSipAmount,
+    };
