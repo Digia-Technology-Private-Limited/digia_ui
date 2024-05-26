390895f6d9da2f75ea54daf9269fa4b0b567161e
amar <amarrawat244@gmail.com>
2024-05-26 15:38:23 +0530

    border radius is now working

--- lib/src/core/utils.dart
+++ lib/src/core/utils.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import '../Utils/basic_shared_utils/dui_decoder.dart';
 import '../Utils/basic_shared_utils/lodash.dart';
-import '../Utils/basic_shared_utils/num_decoder.dart';
 import '../Utils/util_functions.dart';
 import '../components/dui_icons/icon_helpers/icon_data_serialization.dart';
 import 'evaluator.dart';
@@ -30,50 +30,50 @@ Future<Widget?> openDUIPageInBottomSheet({
   Map<String, dynamic>? pageArgs,
 }) {
   return showModalBottomSheet(
+    scrollControlDisabledMaxHeightRatio: 0.7,
     barrierColor:
         eval<String>(style['bgColor'], context: context).letIfTrue(toColor) ??
             Colors.black.withOpacity(0.4),
     context: context,
-    isScrollControlled: true,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(
-        top: Radius.circular(20.0),
-      ),
-    ),
     builder: (ctx) {
-      return SizedBox(
-        height: MediaQuery.of(context).size.height * 0.6,
-        child: Stack(
-          children: [
-            DUIPage(
-              pageUid: pageUid,
-              pageArgs: pageArgs,
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.maybePop(context);
-                  },
-                  child: Container(
-                    height: 24,
-                    width: 24,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.white.withOpacity(0.1),
-                    ),
-                    child: Icon(
-                      getIconData(icondataMap: style['icon']['iconData']),
-                      color: Colors.white.withOpacity(0.5),
-                      size: 16,
+      return Container(
+        color: eval<String>(style['bgColor'], context: context)
+                .letIfTrue(toColor) ??
+            Colors.black.withOpacity(0.4),
+        child: ClipRRect(
+          borderRadius: DUIDecoder.toBorderRadius(style['borderRadius']),
+          child: Stack(
+            children: [
+              DUIPage(
+                pageUid: pageUid,
+                pageArgs: pageArgs,
+              ),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.maybePop(context);
+                    },
+                    child: Container(
+                      height: 24,
+                      width: 24,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white.withOpacity(0.1),
+                      ),
+                      child: Icon(
+                        getIconData(icondataMap: style['icon']['iconData']),
+                        color: Colors.white.withOpacity(0.5),
+                        size: 16,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
     },
