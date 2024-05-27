f07f2c4fce2824124b74466217561a3224b6193d
Premansh Tomar <premanshtomar@gmail.com>
2024-05-27 18:35:10 +0530

    renamed state variable name

--- lib/src/core/builders/dezerv_component/dezerv_dial_pad_builder.dart
+++ lib/src/core/builders/dezerv_component/dezerv_dial_pad_builder.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../json_widget_builder.dart';
 import '../../page/props/dui_widget_json_data.dart';
 import 'dezerv_dial_pad_widget.dart';
-import 'dezerv_dial_pad_widget_props.dart';
 
 class DUIDezervDialPadBuilder extends DUIWidgetBuilder {
   DUIDezervDialPadBuilder(DUIWidgetJsonData data) : super(data: data);
