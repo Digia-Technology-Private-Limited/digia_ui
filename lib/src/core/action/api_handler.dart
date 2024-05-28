66ff8388bd319ac31bdbec7116afd24cd322a7a2
Tushar Gupta <tushar.at.dce@gmail.com>
2024-05-28 20:10:37 +0530

    fixed hydration of body

--- lib/src/core/action/api_handler.dart
+++ lib/src/core/action/api_handler.dart
@@ -18,13 +18,14 @@ class ApiHandler {
     final url = _hydrateTemplate(apiModel.url, args);
     final headers = apiModel.headers?.map((key, value) =>
         MapEntry(_hydrateTemplate(key, args), _hydrateTemplate(value, args)));
+    final body = _hydrateTemplateInDynamic(apiModel.body, args);
 
     final networkClient = DigiaUIClient.getNetworkClient();
     final response = await networkClient.requestProject(
         url: url,
         method: apiModel.method,
         additionalHeaders: headers,
-        data: apiModel.body);
+        data: body);
     return response.data;
   }
 
@@ -34,6 +35,43 @@ class ApiHandler {
       final variableName = match.group(1);
       return values?[variableName]?.toString() ??
           match.group(0)!; // Keep original if value not found
+    });
+  }
+
+  dynamic _hydrateTemplateInDynamic(
+      dynamic json, Map<String, dynamic>? values) {
+    if (json == null) return null;
+
+    if (json is num || json is bool) return json;
+
+    if (json is Map<String, dynamic>) {
+      return json.map((key, value) => MapEntry(
+          _hydrateTemplateInDynamic(key, values),
+          _hydrateTemplateInDynamic(value, values)));
+    }
+
+    if (json is List) {
+      return json.map((e) => _hydrateTemplateInDynamic(json, values)).toList();
+    }
+
+    if (json is! String) return json;
+
+    final regex = RegExp(r'^\{\{(\w+)\}\}$');
+    final match = regex.firstMatch(json);
+    if (match != null) {
+      final variableName = match.group(1);
+
+      return values?[variableName];
+    }
+
+    // Checking for case of String intperpolation
+    final innerVarRegex = RegExp(r'\{\{(\w+)\}\}');
+    final innerVarMatch = innerVarRegex.firstMatch(json);
+    if (innerVarMatch == null) return json;
+
+    return json.replaceAllMapped(innerVarRegex, (match) {
+      final variableName = match.group(1);
+      return values?[variableName]?.toString() ?? match.group(0)!;
     });
   }
 }
