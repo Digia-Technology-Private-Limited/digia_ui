9db0d8ced6d94596028a2dbf0599a180d806c979
Tushar Gupta <tushar.at.dce@gmail.com>
2024-05-30 00:07:04 +0530

    checkbox fixes and text fix in dezerv_stepper

--- lib/src/core/builders/dui_checkbox_builder.dart
+++ lib/src/core/builders/dui_checkbox_builder.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:getwidget/getwidget.dart';
 
 import '../../../digia_ui.dart';
-import '../../Utils/basic_shared_utils/color_decoder.dart';
+import '../../Utils/basic_shared_utils/lodash.dart';
 import '../../Utils/basic_shared_utils/num_decoder.dart';
+import '../../Utils/util_functions.dart';
 import '../evaluator.dart';
 import '../json_widget_builder.dart';
-import 'dui_icon_builder.dart';
 
 class DUICheckboxBuilder extends DUIWidgetBuilder {
   DUICheckboxBuilder({required super.data});
@@ -22,50 +21,62 @@ class DUICheckboxBuilder extends DUIWidgetBuilder {
 
   @override
   Widget build(BuildContext context) {
-    Widget? activeIcon;
-    Widget? inactiveIcon;
+    // Widget? activeIcon;
+    // Widget? inactiveIcon;
     final props = data.props;
 
-    var value = eval<bool>(props['value'], context: context) ?? false;
-    final size = NumDecoder.toDouble(props['size']);
-    final activeBgColor =
-        ColorDecoder.fromHexString(props['activeBgColor'] ?? '#04A24C');
-    final inactiveBgColor =
-        ColorDecoder.fromHexString(props['inactiveBgColor'] ?? '#E0E0E0');
-    final activeIconProps = data.props['activeIcon'] as Map<String, dynamic>?;
+    final value = eval<bool>(props['initialValue'], context: context) ?? false;
 
-    final inactiveIconProps =
-        data.props['inactiveIcon'] as Map<String, dynamic>?;
+    final activeColor = (props['activeColor'] as String?).let(toColor);
+    final checkColor = (props['checkColor'] as String?).let(toColor);
 
-    if (activeIconProps?['iconData'] != null) {
-      activeIcon =
-          DUIIconBuilder.fromProps(props: activeIconProps).build(context);
-    }
-    if (inactiveIconProps?['iconData'] != null) {
-      inactiveIcon =
-          DUIIconBuilder.fromProps(props: inactiveIconProps).build(context);
-    }
+    final size = NumDecoder.toDouble(props['size']) ?? 24;
+    // final uncheckedColor = (props['uncheckedColor'] as String?).let(toColor);
+
+    // final activeIconProps = data.props['activeIcon'] as Map<String, dynamic>?;
+
+    // final inactiveIconProps =
+    // data.props['inactiveIcon'] as Map<String, dynamic>?;
+
+    // if (activeIconProps?['iconData'] != null) {
+    //   activeIcon =
+    //       DUIIconBuilder.fromProps(props: activeIconProps).build(context);
+    // }
+    // if (inactiveIconProps?['iconData'] != null) {
+    //   inactiveIcon =
+    //       DUIIconBuilder.fromProps(props: inactiveIconProps).build(context);
+    // }
 
     return Transform.scale(
-      scale: (size ?? 24) / 24,
-      child: GFCheckbox(
-        type: props['shape'] == 'circle'
-            ? GFCheckboxType.circle
-            : GFCheckboxType.square,
-        activeBgColor: activeBgColor,
-        activeIcon: activeIcon ??
-            const Icon(
-              Icons.check,
-              size: 16,
-            ),
-        inactiveBgColor: inactiveBgColor,
-        inactiveIcon: inactiveIcon,
-        size: size ?? 24,
-        onChanged: (val) {
-          value = eval<bool>(val, context: context) ?? false;
-        },
-        value: value,
-      ),
-    );
+        scale: (size) / 24,
+        child: Checkbox(
+            shape: toButtonShape(data.props['shape']),
+            checkColor: checkColor,
+            activeColor: activeColor,
+            splashRadius: 0,
+            value: value,
+            onChanged: (newValue) {}));
+
+    // return Transform.scale(
+    //   scale: (size ?? 24) / 24,
+    //   child: GFCheckbox(
+    //     type: props['shape'] == 'circle'
+    //         ? GFCheckboxType.circle
+    //         : GFCheckboxType.square,
+    //     activeBgColor: activeBgColor,
+    //     activeIcon: activeIcon ??
+    //         const Icon(
+    //           Icons.check,
+    //           size: 16,
+    //         ),
+    //     inactiveBgColor: inactiveBgColor,
+    //     inactiveIcon: inactiveIcon,
+    //     size: size ?? 24,
+    //     onChanged: (val) {
+    //       value = eval<bool>(val, context: context) ?? false;
+    //     },
+    //     value: value,
+    //   ),
+    // );
   }
 }
