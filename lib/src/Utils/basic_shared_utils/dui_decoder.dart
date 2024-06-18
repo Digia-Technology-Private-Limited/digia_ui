4b677234a78807ffd36386414592cd1b1f08ae48
Tushar Gupta <tushar.at.dce@gmail.com>
2024-06-18 13:27:21 +0530

    should be dynamic

--- lib/src/Utils/basic_shared_utils/dui_decoder.dart
+++ lib/src/Utils/basic_shared_utils/dui_decoder.dart
@@ -176,7 +176,7 @@ class DUIDecoder {
     return null;
   }
 
-  static BoxFit toBoxFit(String? fitValue) => switch (fitValue) {
+  static BoxFit toBoxFit(dynamic fitValue) => switch (fitValue) {
         'fill' => BoxFit.fill,
         'contain' => BoxFit.contain,
         'cover' => BoxFit.cover,
@@ -530,7 +530,7 @@ class DUIDecoder {
     };
   }
 
-  static Clip toClip(String? value, {Clip defaultValue = Clip.none}) {
+  static Clip toClip(dynamic value, {Clip defaultValue = Clip.none}) {
     if (value == null) return defaultValue;
 
     return switch (value) {
