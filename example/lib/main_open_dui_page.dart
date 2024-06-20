287bcbbd8252c6826d7e22cfadf1ac536dbf2260
Tushar Gupta <tushar.at.dce@gmail.com>
2024-06-20 23:49:11 +0530

    lint fix

--- example/lib/main_open_dui_page.dart
+++ example/lib/main_open_dui_page.dart
@@ -6,7 +6,7 @@ const String baseUrl = 'https://app.digia.tech/api/v1';
 
 void main() async {
   await DigiaUIClient.initializeFromData(
-    baseUrl: baseUrl,
+      baseUrl: baseUrl,
       accessKey: "",
       data: const {
         'theme': <String, dynamic>{
