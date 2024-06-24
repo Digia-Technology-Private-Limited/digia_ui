8a5fe91ed7b7947028be4f847c28aac08dbf4419
amar <amarrawat244@gmail.com>
2024-06-24 17:23:18 +0530

    added progress value in circular progress indicator

--- lib/src/core/builders/dui_circular_progress_indicator_builder.dart
+++ lib/src/core/builders/dui_circular_progress_indicator_builder.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../Utils/basic_shared_utils/dui_decoder.dart';
-import '../../Utils/basic_shared_utils/lodash.dart';
 import '../../Utils/util_functions.dart';
 import '../evaluator.dart';
 import '../json_widget_builder.dart';
@@ -16,18 +15,23 @@ class DUICircularProgressBarBuilder extends DUIWidgetBuilder {
 
   @override
   Widget build(BuildContext context) {
-    final bgColor = eval<String>(data.props['bgColor'], context: context)
-        .letIfTrue(toColor);
+    final bgColor =
+        makeColor(eval<String>(data.props['bgColor'], context: context));
+
     final indicatorColor =
-        eval<String>(data.props['indicatorColor'], context: context)
-            .letIfTrue(toColor);
+        makeColor(eval<String>(data.props['indicatorColor'], context: context));
+
     final strokeWidth =
         eval<double>(data.props['strokeWidth'], context: context);
 
     final strokeAlign =
         eval<double>(data.props['stokeAlign'], context: context);
 
+    final progressValue =
+        eval<double>(data.props['progressValue'] / 100, context: context);
+
     return CircularProgressIndicator(
+      value: progressValue,
       backgroundColor: bgColor,
       color: indicatorColor,
       strokeWidth: strokeWidth ?? 4.0,
