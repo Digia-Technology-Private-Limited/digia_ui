f7250885d6a8e2c30f7c00bb01a3513192cc7529
Tushar Gupta <tushar.at.dce@gmail.com>
2024-07-15 23:11:37 +0530

    exposed openDUIPage

--- lib/digia_ui.dart
+++ lib/digia_ui.dart
@@ -5,6 +5,7 @@ export 'package:digia_ui/src/components/dui_widget.dart';
 export 'package:digia_ui/src/config_resolver.dart';
 export 'package:digia_ui/src/core/page/dui_page.dart';
 export 'package:digia_ui/src/core/page/props/dui_widget_json_data.dart';
+export 'package:digia_ui/src/core/utils.dart';
 export 'package:digia_ui/src/digia_ui_client.dart';
 export 'package:digia_ui/src/digia_ui_provider.dart';
 export 'package:digia_ui/src/dui_app.dart';
