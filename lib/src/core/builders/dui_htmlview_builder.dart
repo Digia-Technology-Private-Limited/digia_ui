a92ec8b3aa7717502f9300ac07f9c0c2d1c6e99d
Tushar Gupta <tushar.at.dce@gmail.com>
2024-07-23 04:38:09 +0530

    overriding body, span, para is sufficient for now

--- lib/src/core/builders/dui_htmlview_builder.dart
+++ lib/src/core/builders/dui_htmlview_builder.dart
@@ -17,76 +17,51 @@ class DUIHtmlViewBuilder extends DUIWidgetBuilder {
 
   @override
   Widget build(BuildContext context) {
-    final height = eval<double>(data.props['height'], context: context);
-    final width = eval<double>(data.props['width'], context: context);
-    final verticalAlign = toVerticalAlign(
-        eval<String>(data.props['verticalAlign'], context: context));
-    final alignment = DUIDecoder.toAlignment(
-        eval<String>(data.props['alignment'], context: context));
-    final maxLines = eval<int>(data.props['maxLines'], context: context);
-    final textOverflow = DUIDecoder.toTextOverflow(
-        eval<String>(data.props['textOverflow'], context: context));
-
-    final googleFont = (data.props['fontFamily'] as String?)?.let(
-      (p0) => GoogleFonts.getFont(p0,
-          fontSize: eval<double>(data.props['fontSize'], context: context),
-          fontStyle: DUIDecoder.toFontStyle(
-              eval<String>(data.props['fontStyle'], context: context)),
-          fontWeight: DUIDecoder.toFontWeight(
-              eval<String>(data.props['fontWeight'], context: context)),
-          height: eval<double>(data.props['lineHeight'], context: context),
-          backgroundColor: makeColor(
-              eval<String>(data.props['backgroundColor'], context: context)),
-          color: makeColor(eval<String>(data.props['color'], context: context)),
-          decoration: DUIDecoder.toTextDecoration(
-              eval<String>(data.props['textDecoration'], context: context)),
-          decorationColor: makeColor(eval<String>(
-              data.props['textDecorationColor'],
-              context: context)),
-          decorationStyle: DUIDecoder.toTextDecorationStyle(eval<String>(
-              data.props['textDecorationStyle'],
-              context: context)),
-          decorationThickness: eval<double>(data.props['textDecorationThickness'], context: context)),
-    );
-
-    Style makeStyle() {
-      final style = Style(
-          height: height != null ? Height(height) : null,
-          width: width != null ? Width(width) : null,
-          verticalAlign: verticalAlign,
-          alignment: alignment,
-          maxLines: maxLines,
-          textOverflow: textOverflow);
-
-      if (googleFont == null) return style;
-
-      return style.merge(Style.fromTextStyle(googleFont));
-    }
-
     return Html(
       data: eval<String>(data.props['content'], context: context),
       shrinkWrap:
           eval<bool>(data.props['shrinkWrap'], context: context) ?? false,
       style: {
-        'b': makeStyle(),
-        'span': makeStyle(),
-        'sup': makeStyle(),
-        'p': makeStyle(),
+        'body': Style(padding: HtmlPaddings.all(0.0), margin: Margins.all(0.0))
+            .merge(_makeStyle(context, data.props['htmlStyleOverridesBody'])),
+        'span': _makeStyle(context, data.props['htmlStyleOverridesSpan']),
+        'p': _makeStyle(context, data.props['htmlStyleOverridesParagraph']),
       },
     );
   }
 }
 
-VerticalAlign toVerticalAlign(String? value) {
-  if (value == null) return VerticalAlign.baseline;
+Style _makeStyle(BuildContext context, Map<String, dynamic>? styleMap) {
+  if (styleMap == null) return Style();
 
-  return switch (value) {
-    'baseline' => VerticalAlign.baseline,
-    'sub' => VerticalAlign.sub,
-    'sup' => VerticalAlign.sup,
-    'top' => VerticalAlign.top,
-    'bottom' => VerticalAlign.bottom,
-    'middle' => VerticalAlign.middle,
-    _ => VerticalAlign.baseline
-  };
+  final maxLines = eval<int>(styleMap['maxLines'], context: context);
+  final textOverflow = DUIDecoder.toTextOverflow(
+      eval<String>(styleMap['textOverflow'], context: context));
+
+  final googleFont = (styleMap['fontFamily'] as String?)?.let(
+    (p0) => GoogleFonts.getFont(p0,
+        fontSize: eval<double>(styleMap['fontSize'], context: context),
+        fontStyle: DUIDecoder.toFontStyle(
+            eval<String>(styleMap['fontStyle'], context: context)),
+        fontWeight: DUIDecoder.toFontWeight(
+            eval<String>(styleMap['fontWeight'], context: context)),
+        height: eval<double>(styleMap['lineHeight'], context: context),
+        backgroundColor: makeColor(
+            eval<String>(styleMap['backgroundColor'], context: context)),
+        color: makeColor(eval<String>(styleMap['color'], context: context)),
+        decoration: DUIDecoder.toTextDecoration(
+            eval<String>(styleMap['textDecoration'], context: context)),
+        decorationColor: makeColor(
+            eval<String>(styleMap['textDecorationColor'], context: context)),
+        decorationStyle: DUIDecoder.toTextDecorationStyle(
+            eval<String>(styleMap['textDecorationStyle'], context: context)),
+        decorationThickness: eval<double>(styleMap['textDecorationThickness'],
+            context: context)),
+  );
+
+  final style = Style(maxLines: maxLines, textOverflow: textOverflow);
+
+  if (googleFont == null) return style;
+
+  return style.merge(Style.fromTextStyle(googleFont));
 }
