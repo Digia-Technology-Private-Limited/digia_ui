a71aeb85bb6c1afb9e48fe6775b72d2454ac9b57
Tushar Gupta <tushar.at.dce@gmail.com>
2024-08-09 13:58:13 +0530

    code refactoring

--- lib/src/core/builders/dui_line_chart_builder.dart
+++ lib/src/core/builders/dui_line_chart_builder.dart
@@ -1,10 +1,10 @@
-import 'dart:convert';
-
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 import '../../../digia_ui.dart';
 import '../../Utils/basic_shared_utils/lodash.dart';
+import '../../Utils/basic_shared_utils/num_decoder.dart';
+import '../../Utils/extensions.dart';
 import '../../Utils/util_functions.dart';
 import '../../core/page/props/dui_widget_json_data.dart';
 import '../evaluator.dart';
@@ -25,96 +25,102 @@ class DUILineChartBuilder extends DUIWidgetBuilder {
 
   @override
   Widget build(BuildContext context) {
-    bool extraLines =
-        eval<bool>(data.props['extraLines']['show'], context: context) ?? false;
-
-    List<HorizontalLine>? horizontalLines = eval<List>(
-            data.props['extraLines']['horizontalLines'],
-            context: context)
-        ?.map((e) {
-      return HorizontalLine(
-        y: eval<double>(e['linePoint'], context: context) ?? 0,
-        color: makeColor(eval<String>(e['color'], context: context)),
-        strokeWidth: eval<double>(e['strokeWidth'], context: context) ?? 2,
-        dashArray: ifNotNull2(
-            eval<int>(e['dashArray']['length'], context: context)!,
-            eval<int>(e['dashArray']['gap'], context: context)!,
-            (p0, p1) => [p0, p1]),
-      );
-    }).toList();
-    List<VerticalLine>? verticalLines =
-        eval<List>(data.props['extraLines']['verticalLines'], context: context)
-            ?.map((e) {
-      return VerticalLine(
-        x: eval<double>(e['linePoint'], context: context) ?? 0,
-        color: makeColor(eval<String>(e['color'], context: context)),
-        strokeWidth: eval<double>(e['strokeWidth'], context: context) ?? 2,
-        dashArray: ifNotNull2(
-            eval<int>(e['dashArray']['length'], context: context)!,
-            eval<int>(e['dashArray']['gap'], context: context)!,
-            (p0, p1) => [p0, p1]),
-      );
-    }).toList();
-
-
-
-    dynamic safeJsonDecode(String input) {
-      try {
-        return jsonDecode(input);
-      } on FormatException catch (e) {
-        print('Invalid JSON format: $e ${ eval(input, context: context)}');
-        return  eval(input, context: context);
-      } on TypeError catch (e) {
-        print('Type error: $e  ${ eval(input, context: context)}');
-        return  eval(input, context: context);
-      } catch (e) {
-        print('Unknown error: $e  ${ eval(input, context: context)}');
-        return  eval(input, context: context);
-      }
-    }
-
     final LineChartData chartData = LineChartData(
-      extraLinesData: extraLines
-          ? ExtraLinesData(
-              horizontalLines: horizontalLines ?? [],
-              verticalLines: verticalLines ?? [],
-            )
-          : null,
+      extraLinesData: _toExtraLinesData(
+          context, tryCast<Map<String, dynamic>>(data.props['extraLines'])),
       titlesData: const FlTitlesData(show: false),
-      lineBarsData: eval<List>(data.props['lines'], context: context)?.map((e) {
-//function => try catch =>
-            final List? spots =
-                safeJsonDecode(e['spots']);
-            return LineChartBarData(
-              isStepLineChart: false,
-              spots: spots?.map((s) {
-                    return FlSpot(s['x'].toDouble(), s['y'].toDouble());
-                  }).toList() ??
-                  [],
-              isCurved: eval<bool>(e['isCurved'], context: context) ?? false,
-              barWidth: eval<double>(e['barWidth'], context: context) ?? 2,
-              color: makeColor(eval<String>(e['lineColor'], context: context)),
-              belowBarData: BarAreaData(
-                  show:
-                      eval<bool>(e['belowBarData']['show'], context: context) ??
-                          false,
-                  color: makeColor(eval<String>(e['belowBarData']['color'],
-                      context: context)),
-                  gradient: toGradiant(
-                      eval<Map<String, dynamic>>(e['belowBarData']['gradiant'],
-                          context: context),
-                      context)),
-            );
-          }).toList() ??
-          [],
-      borderData: FlBorderData(
-        show: false,
-      ),
-      gridData: const FlGridData(show: false),
+      lineBarsData:
+          _toLineBarsData(context, tryCast<List>(data.props['lines'])) ?? [],
     );
 
-    return LineChart(
-      chartData,
-    );
+    return LineChart(chartData);
   }
+}
+
+List<dynamic>? _getSpots(BuildContext context, dynamic input) {
+  if (input == null) return null;
+
+  if (input is List) return input;
+
+  if (input is! String) return null;
+
+  return tryJsonDecode(input) ?? eval<List>(input, context: context);
+}
+
+List<LineChartBarData>? _toLineBarsData(
+    BuildContext context, List<dynamic>? lines) {
+  if (lines == null || lines.isEmpty) return null;
+
+  return lines.map(
+    (line) {
+      final List<FlSpot>? spots = _getSpots(context, line['spots'])
+          ?.map((s) => ifNotNull2(NumDecoder.toDouble(s['x']),
+              NumDecoder.toDouble(s['y']), (x, y) => FlSpot(x, y)))
+          .nonNulls
+          .toList();
+      return LineChartBarData(
+        spots: spots ?? [],
+        isCurved: eval<bool>(line['isCurved'], context: context) ?? false,
+        barWidth: eval<double>(line['barWidth'], context: context) ?? 2,
+        color: makeColor(eval<String>(line['lineColor'], context: context)),
+        belowBarData: _toBelowbarData(
+            context, tryCast<Map<String, dynamic>>(line['belowBarData'])),
+      );
+    },
+  ).toList();
+}
+
+BarAreaData? _toBelowbarData(
+    BuildContext context, Map<String, dynamic>? props) {
+  if (props == null || props.isEmpty) return null;
+
+  return BarAreaData(
+      show: eval<bool>(props['show'], context: context) ?? false,
+      color: makeColor(eval<String>(props['color'], context: context)),
+      gradient: toGradiant(
+          tryCast<Map<String, dynamic>>(props['gradiant']), context));
+}
+
+ExtraLinesData? _toExtraLinesData(
+    BuildContext context, Map<String, dynamic>? props) {
+  if (props == null || props.isEmpty) return null;
+
+  final showExtraLines = eval<bool>(props.valueFor(keyPath: 'extralines.show'),
+          context: context) ??
+      false;
+
+  if (!showExtraLines) return null;
+
+  List<int>? toDashArray(dynamic dashArray) {
+    return ifNotNull2(eval<int>(dashArray['length'], context: context)!,
+        eval<int>(dashArray['gap'], context: context)!, (p0, p1) => [p0, p1]);
+  }
+
+  List<HorizontalLine>? horizontalLines = eval<List>(
+          props.valueFor(keyPath: 'extraLines.horizontalLines'),
+          context: context)
+      ?.map((e) {
+    return HorizontalLine(
+      y: eval<double>(e['linePoint'], context: context) ?? 0,
+      color: makeColor(eval<String>(e['color'], context: context)),
+      strokeWidth: eval<double>(e['strokeWidth'], context: context) ?? 2,
+      dashArray: toDashArray(e['dashArray']),
+    );
+  }).toList();
+
+  List<VerticalLine>? verticalLines = eval<List>(
+          props.valueFor(keyPath: 'extraLines.verticalLines'),
+          context: context)
+      ?.map((e) {
+    return VerticalLine(
+      x: eval<double>(e['linePoint'], context: context) ?? 0,
+      color: makeColor(eval<String>(e['color'], context: context)),
+      strokeWidth: eval<double>(e['strokeWidth'], context: context) ?? 2,
+      dashArray: toDashArray(e['dashArray']),
+    );
+  }).toList();
+
+  return ExtraLinesData(
+      horizontalLines: horizontalLines ?? [],
+      verticalLines: verticalLines ?? []);
 }
