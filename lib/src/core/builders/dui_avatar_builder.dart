47d5fccb8631117e1b9542d9c1092db8c746cc17
Anupam Singh <anupams@DEZ-BLR-MAC002.local>
2024-08-21 15:38:24 +0530

    init for fixing avatar

--- lib/src/core/builders/dui_avatar_builder.dart
+++ lib/src/core/builders/dui_avatar_builder.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
-import '../../components/dui_avatar/avatar.dart';
-import '../../components/dui_avatar/avatar_props.dart';
+import '../../Utils/basic_shared_utils/dui_decoder.dart';
+import '../../Utils/util_functions.dart';
+import '../evaluator.dart';
 import '../json_widget_builder.dart';
 import '../page/props/dui_widget_json_data.dart';
+import 'dui_image_builder.dart';
+import 'dui_text_builder.dart';
 
 class DUIAvatarBuilder extends DUIWidgetBuilder {
   DUIAvatarBuilder({required super.data});
@@ -12,10 +15,75 @@ class DUIAvatarBuilder extends DUIWidgetBuilder {
     return DUIAvatarBuilder(data: data);
   }
 
+  factory DUIAvatarBuilder.fromProps(Map<String, dynamic> props) {
+    return DUIAvatarBuilder(
+      data: DUIWidgetJsonData(type: 'digia/avatar', props: props),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DUIAvatar(
-      props: DUIAvatarProps.fromJson(data.props),
+    final shape =
+        eval<Map<String, dynamic>>(data.props['shape'], context: context);
+
+    return switch (shape!['value']) {
+      'circle' => _getCircleAvatar(shape, context),
+      'square' => _getSquareAvatar(shape, context),
+      _ => _getCircleAvatar(shape, context)
+    };
+  }
+
+  Widget _getCircleAvatar(Map<String, dynamic> shape, BuildContext context) {
+    final bgColor = eval<String>(data.props['bgColor'], context: context);
+    final radius = eval<double>(shape[''], context: context);
+
+    return Container(
+      height: (radius ?? 16) * 2,
+      width: (radius ?? 16) * 2,
+      decoration: BoxDecoration(
+        color: makeColor(bgColor) ?? Colors.grey,
+        shape: BoxShape.circle,
+      ),
+      clipBehavior: Clip.hardEdge,
+      child: _getAvatarChildWidget(context),
     );
+  }
+
+  Widget _getSquareAvatar(Map<String, dynamic> shape, BuildContext context) {
+    final String? bgColor =
+        eval<String>(data.props['bgColor'], context: context);
+    final String? cornerRadius =
+        eval<String>(shape['cornerRadius'], context: context);
+    final double? side = eval<double>(shape['side'], context: context);
+
+    return Container(
+      height: side,
+      width: side,
+      decoration: BoxDecoration(
+          color: makeColor(bgColor) ?? Colors.grey,
+          shape: BoxShape.rectangle,
+          borderRadius: DUIDecoder.toBorderRadius(cornerRadius)),
+      clipBehavior: Clip.hardEdge,
+      child: _getAvatarChildWidget(context),
+    );
+  }
+
+  Widget? _getAvatarChildWidget(BuildContext context) {
+    final String? imageSrc =
+        eval<String>(data.props['imageSrc'], context: context);
+    final String? imageFit =
+        eval<String>(data.props['imageFit'], context: context);
+
+    if (imageSrc != null) {
+      return DUIImageBuilder.fromProps(
+          props: {'imageSrc': imageSrc, 'fit': imageFit}).build(context);
+    }
+    // final Map<String, dynamic>? text =
+    //     data.props['text'].tryCast<Map<String, dynamic>>();
+    return Align(
+        alignment: Alignment.center,
+        child: DUITextBuilder.fromProps(
+                props: data.props['text'] as Map<String, dynamic>?)
+            .build(context));
   }
 }
