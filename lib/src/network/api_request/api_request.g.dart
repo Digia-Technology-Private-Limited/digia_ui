580febe67950eb0f62b0942ac176f7021f0ad5cb
vivek singh <viveks@DEZ-BLR-MAC006.local>
2024-08-23 18:00:07 +0530

    made bodyType nullable

--- lib/src/network/api_request/api_request.g.dart
+++ lib/src/network/api_request/api_request.g.dart
@@ -12,7 +12,7 @@ APIModel _$APIModelFromJson(Map<String, dynamic> json) => APIModel(
       method: $enumDecode(_$HttpMethodEnumMap, json['method']),
       headers: json['headers'] as Map<String, dynamic>?,
       body: json['body'] as Map<String, dynamic>?,
-      bodyType: $enumDecode(_$BodyTypeEnumMap, json['bodyType']),
+      bodyType: $enumDecodeNullable(_$BodyTypeEnumMap, json['bodyType']),
       variables: _$JsonConverterFromJson<Map<String, dynamic>,
               Map<String, VariableDef>>(
           json['variables'], const VariablesJsonConverter().fromJson),
@@ -24,7 +24,7 @@ Map<String, dynamic> _$APIModelToJson(APIModel instance) => <String, dynamic>{
       'method': _$HttpMethodEnumMap[instance.method]!,
       'headers': instance.headers,
       'body': instance.body,
-      'bodyType': _$BodyTypeEnumMap[instance.bodyType]!,
+      'bodyType': _$BodyTypeEnumMap[instance.bodyType],
       'variables':
           _$JsonConverterToJson<Map<String, dynamic>, Map<String, VariableDef>>(
               instance.variables, const VariablesJsonConverter().toJson),
