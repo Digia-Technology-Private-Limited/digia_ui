aa719676ceb1dd2d2ca9100f2aa1369b9d535de6
Tushar Gupta <tushar.at.dce@gmail.com>
2024-09-17 16:02:35 +0530

    name change for JsonMap to JsonLike

--- lib/src/framework/models/vw_node_data.dart
+++ lib/src/framework/models/vw_node_data.dart
@@ -1,7 +1,6 @@
-import 'package:derry/utils.dart';
-
 import '../utils/functional_util.dart';
 import '../utils/json_util.dart';
+import '../utils/type_aliases.dart';
 import 'props.dart';
 import 'vw_repeat_data.dart';
 
@@ -28,10 +27,10 @@ class VWNodeData {
     return VWNodeData(
         category: as$<String>(json['category']) ?? '',
         type: as$<String>(json['type']) ?? '',
-        props: as$<JsonMap>(json['props']).maybe((p0) => Props(p0)) ??
+        props: as$<JsonLike>(json['props']).maybe((p0) => Props(p0)) ??
             Props.empty(),
         commonProps:
-            as$<JsonMap>(json['containerProps']).maybe((p0) => Props(p0)),
+            as$<JsonLike>(json['containerProps']).maybe((p0) => Props(p0)),
         childGroups: tryKeys(
           json,
           ['children', 'composites', 'childGroups'],
@@ -42,7 +41,7 @@ class VWNodeData {
   }
 
   static Map<String, List<VWNodeData>>? _parseVWNodeDataMap(dynamic json) {
-    final jsonMap = as$<JsonMap>(json);
+    final jsonMap = as$<JsonLike>(json);
     if (jsonMap == null) return null;
 
     return Map.fromEntries(
@@ -50,8 +49,8 @@ class VWNodeData {
         final key = entry.key;
         final value = as$<List>(entry.value);
         final nodeDataList = value
-            ?.map((item) => as$<JsonMap>(item))
-            .whereType<JsonMap>()
+            ?.map((item) => as$<JsonLike>(item))
+            .whereType<JsonLike>()
             .map(VWNodeData.fromJson)
             .toList();
 
