cfa4092375c23cc29009ca020fb2b512619ac088
Ramsthr <ramsthr38@gmail.com>
2024-10-03 15:44:02 +0530

    html fixes

--- lib/src/framework/widgets/html_view.dart
+++ lib/src/framework/widgets/html_view.dart
@@ -18,8 +18,12 @@ class VWHtmlView extends VirtualLeafStatelessWidget<Props> {
 
   @override
   Widget render(RenderPayload payload) {
+    String defaultData = '';
+    if (props.isEmpty) {
+      defaultData = ('<h1>H1 Header</h1> <h2>H2 Header</h2> <p>Paragraph</p>');
+    }
     return Html(
-      data: payload.eval<String>(props.get('content')),
+      data: payload.eval<String>(props.get('content')) ?? defaultData,
       shrinkWrap: payload.eval<bool>(props.get('shrinkWrap')) ?? false,
       style: {
         'body': Style(padding: HtmlPaddings.all(0.0), margin: Margins.all(0.0))
@@ -40,27 +44,46 @@ Style _makeStyle(RenderPayload payload, Props styleMap) {
   final maxLines = payload.eval<int>(styleMap.get('maxLines'));
   final textOverflow =
       To.textOverflow(payload.eval<String>(styleMap.get('textOverflow')));
+  final fontSize = payload.eval<double>(styleMap.get('fontSize'));
+  final fontStyle =
+      To.fontStyle(payload.eval<String>(styleMap.get('fontStyle')));
+  final fontWeight =
+      To.fontWeight(payload.eval<String>(styleMap.get('fontWeight')));
+  final height = payload.eval<double>(styleMap.get('lineHeight'));
+  final backgroundColor = payload.evalColor(styleMap.get('backgroundColor'));
+  final color = payload.evalColor(styleMap.get('color'));
+  final decoration =
+      To.textDecoration(payload.eval<String>(styleMap.get('textDecoration')));
+  final decorationColor =
+      payload.evalColor(styleMap.get('textDecorationColor'));
+  final decorationStyle = To.textDecorationStyle(
+      payload.eval<String>(styleMap.get('textDecorationStyle')));
+  final decorationThickness =
+      payload.eval<double>(styleMap.get('textDecorationThickness'));
 
-  final googleFont = (styleMap.getString('fontFamily')).maybe(
-    (p0) => GoogleFonts.getFont(p0,
-        fontSize: payload.eval<double>(styleMap.get('fontSize')),
-        fontStyle:
-            To.fontStyle(payload.eval<String>(styleMap.get('fontStyle'))),
-        fontWeight:
-            To.fontWeight(payload.eval<String>(styleMap.get('fontWeight'))),
-        height: payload.eval<double>(styleMap.get('lineHeight')),
-        backgroundColor: payload.evalColor(styleMap.get('backgroundColor')),
-        color: payload.evalColor(styleMap.get('color')),
-        decoration: To.textDecoration(
-            payload.eval<String>(styleMap.get('textDecoration'))),
-        decorationColor: payload.evalColor(styleMap.get('textDecorationColor')),
-        decorationStyle: To.textDecorationStyle(
-            payload.eval<String>(styleMap.get('textDecorationStyle'))),
-        decorationThickness:
-            payload.eval<double>(styleMap.get('textDecorationThickness'))),
+  final googleFont = (styleMap.getString('fontFamily')).maybe((p0) {
+    if (styleMap.getString('fontFamily')!.isEmpty) {
+      return null;
+    }
+    return GoogleFonts.getFont(
+      p0,
+      fontSize: fontSize,
+      height: height,
+    );
+  });
+
+  final style = Style(
+    maxLines: maxLines,
+    textOverflow: textOverflow,
+    fontStyle: fontStyle,
+    fontWeight: fontWeight,
+    backgroundColor: backgroundColor,
+    color: color,
+    textDecoration: decoration,
+    textDecorationColor: decorationColor,
+    textDecorationStyle: decorationStyle,
+    textDecorationThickness: decorationThickness,
   );
-
-  final style = Style(maxLines: maxLines, textOverflow: textOverflow);
 
   if (googleFont == null) return style;
 
