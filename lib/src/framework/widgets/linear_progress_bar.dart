f398322c5ad75c9da2cf8b7f1960ec9c7cf31b60
amarrawat <amarrawat@digia.tech>
2024-11-18 17:41:39 +0530

    fixed animate from last and removed unwanted height

--- lib/src/framework/widgets/linear_progress_bar.dart
+++ lib/src/framework/widgets/linear_progress_bar.dart
@@ -16,18 +16,20 @@ class VWLinearProgressBar extends VirtualLeafStatelessWidget<Props> {
     final progressValue = payload.eval<double>(props.get('progressValue'));
     final isReverse = payload.eval<bool>(props.get('isReverse')) ?? false;
     final String type = props.getString('type') ?? 'indeterminate';
+    final animateFromLastPercent =
+        payload.eval<bool>(props.get('animateFromLastPercent')) ?? false;
 
     return RotatedBox(
       quarterTurns: isReverse ? 2 : 0,
-      child: _getChild(progressValue, type, payload),
+      child: _getChild(progressValue, type, payload, animateFromLastPercent),
     );
   }
 
-  Widget _getChild(double? progressValue, String? type, RenderPayload payload) {
+  Widget _getChild(double? progressValue, String? type, RenderPayload payload,
+      bool animateFromLastPercent) {
     if (type == 'indeterminate') {
       return SizedBox(
         width: props.getDouble('width'),
-        height: props.getDouble('thickness') ?? 5.0,
         child: LinearProgressIndicator(
           color: payload.evalColor(props.get('indicatorColor')) ?? Colors.blue,
           backgroundColor:
@@ -41,16 +43,20 @@ class VWLinearProgressBar extends VirtualLeafStatelessWidget<Props> {
     } else {
       return SizedBox(
         width: props.getDouble('width'),
-        height: props.getDouble('thickness') ?? 5.0,
         child: TweenAnimationBuilder<double>(
-          tween: Tween(
-            begin: 0.0,
-            end: progressValue != null ? progressValue / 100.0 : 0,
-          ),
+          tween: animateFromLastPercent
+              ? Tween(
+                  begin: progressValue != null ? progressValue / 100.0 : 0,
+                  end: 0.0,
+                )
+              : Tween(
+                  begin: 0.0,
+                  end: progressValue != null ? progressValue / 100.0 : 0,
+                ),
           duration: const Duration(milliseconds: 500),
           builder: (context, value, child) {
             return LinearProgressIndicator(
-              minHeight: props.getDouble('thickness') ?? 5.0,
+              minHeight: props.getDouble('thickness'),
               borderRadius: BorderRadius.all(
                 Radius.circular(
                   props.getDouble('borderRadius') ?? 0.0,
