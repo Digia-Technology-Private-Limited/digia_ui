7e1b2a59facd6fd60652537653cc0a30a395b0ca
Ramsthr <ramsthr38@gmail.com>
2025-01-11 22:46:37 +0530

    added indicator

--- lib/src/framework/widgets/carousel.dart
+++ lib/src/framework/widgets/carousel.dart
@@ -1,8 +1,9 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import '../base/virtual_stateless_widget.dart';
+import '../base/virtual_widget.dart';
 import '../expr/default_scope_context.dart';
 import '../expr/scope_context.dart';
+import '../internal_widgets/internal_carousel.dart';
 import '../render_payload.dart';
 import '../utils/flutter_extensions.dart';
 import '../widget_props/carousel_props.dart';
@@ -21,67 +22,63 @@ class VWCarousel extends VirtualStatelessWidget<CarouselProps> {
   @override
   Widget render(RenderPayload payload) {
     if (children == null || children!.isEmpty) return empty();
+
     if (shouldRepeatChild) {
       final childToRepeat = children!.first;
       final items = payload.evalRepeatData(repeatData!);
-      return SizedBox(
-        height: props.height?.toHeight(payload.buildContext),
-        width: props.width?.toWidth(payload.buildContext) ?? double.infinity,
-        child: CarouselSlider.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index, realIndex) {
-            return childToRepeat.toWidget(
-              payload.copyWithChainedContext(
-                _createExprContext(items[realIndex], realIndex),
-              ),
-            );
-          },
-          options: CarouselOptions(
-            scrollDirection: props.direction,
-            aspectRatio: props.aspectRatio,
-            autoPlay: props.autoPlay,
-            autoPlayAnimationDuration:
-                Duration(milliseconds: props.animationDuration),
-            autoPlayCurve: Curves.linear,
-            autoPlayInterval: Duration(milliseconds: props.autoPlayInterval),
-            enableInfiniteScroll: props.infiniteScroll,
-            initialPage: props.initialPage,
-            viewportFraction: props.viewportFraction,
-            enlargeFactor: props.enlargeFactor,
-            enlargeCenterPage: props.enlargeCenterPage,
-            reverse: props.reverseScroll,
+      return InternalCarousel(
+        itemCount: items.length,
+        itemBuilder: (buildContext, index) => childToRepeat.toWidget(
+          payload.copyWithChainedContext(
+            _createExprContext(items[index], index),
           ),
         ),
-      );
-    } else {
-      return SizedBox(
-        height: props.height?.toHeight(payload.buildContext),
         width: props.width?.toWidth(payload.buildContext) ?? double.infinity,
-        child: CarouselSlider.builder(
-          itemCount: children?.length,
-          itemBuilder: (context, index, realIndex) {
-            return children![realIndex].toWidget(payload);
-          },
-          options: CarouselOptions(
-            scrollDirection: props.direction,
-            aspectRatio: props.aspectRatio,
-            autoPlay: props.autoPlay,
-            autoPlayAnimationDuration:
-                Duration(milliseconds: props.animationDuration),
-            autoPlayCurve: Curves.linear,
-            autoPlayInterval: Duration(
-              milliseconds: props.autoPlayInterval,
-            ),
-            enableInfiniteScroll: props.infiniteScroll,
-            initialPage: props.initialPage,
-            viewportFraction: props.viewportFraction,
-            enlargeFactor: props.enlargeFactor,
-            enlargeCenterPage: props.enlargeCenterPage,
-            reverse: props.reverseScroll,
-          ),
-        ),
+        height: props.height?.toHeight(payload.buildContext),
+        direction: props.direction,
+        aspectRatio: props.aspectRatio,
+        autoPlay: props.autoPlay,
+        showIndicator: props.showIndicator,
+        animationDuration: props.animationDuration,
+        autoPlayInterval: props.autoPlayInterval,
+        infiniteScroll: props.infiniteScroll,
+        initialPage: props.initialPage,
+        viewportFraction: props.viewportFraction,
+        enlargeFactor: props.enlargeFactor,
+        enlargeCenterPage: props.enlargeCenterPage,
+        reverseScroll: props.reverseScroll,
+        offset: props.offset,
+        dotHeight: props.dotHeight,
+        dotWidth: props.dotWidth,
+        spacing: props.spacing,
+        dotColor: payload.evalColorExpr(props.dotColor) ?? Colors.grey,
+        activeDotColor:
+            payload.evalColorExpr(props.activeDotColor) ?? Colors.indigo,
       );
     }
+    return InternalCarousel(
+      children: children?.toWidgetArray(payload) ?? [],
+      width: props.width?.toWidth(payload.buildContext) ?? double.infinity,
+      height: props.height?.toHeight(payload.buildContext),
+      direction: props.direction,
+      aspectRatio: props.aspectRatio,
+      autoPlay: props.autoPlay,
+      animationDuration: props.animationDuration,
+      autoPlayInterval: props.autoPlayInterval,
+      infiniteScroll: props.infiniteScroll,
+      initialPage: props.initialPage,
+      viewportFraction: props.viewportFraction,
+      enlargeFactor: props.enlargeFactor,
+      enlargeCenterPage: props.enlargeCenterPage,
+      reverseScroll: props.reverseScroll,
+      offset: props.offset,
+      dotHeight: props.dotHeight,
+      dotWidth: props.dotWidth,
+      spacing: props.spacing,
+      dotColor: payload.evalColorExpr(props.dotColor) ?? Colors.grey,
+      activeDotColor:
+          payload.evalColorExpr(props.activeDotColor) ?? Colors.indigo,
+    );
   }
 
   ScopeContext _createExprContext(Object? item, int index) {
@@ -91,4 +88,8 @@ class VWCarousel extends VirtualStatelessWidget<CarouselProps> {
       // TODO: Add class instance using refName
     });
   }
+}
+
+extension on List<VirtualWidget>? {
+  toWidgetArray(RenderPayload payload) {}
 }
