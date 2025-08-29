79f3ee125f3690c44fec4300377ff9e8e78466c1
Ramsthr <ramsuthar@digia.tech>
2025-08-29 13:45:23 +0530

    added bodytype=graphql

--- lib/src/network/api_request/api_request.g.dart
+++ lib/src/network/api_request/api_request.g.dart
@@ -7,28 +7,32 @@ part of 'api_request.dart';
 // **************************************************************************
 
 APIModel _$APIModelFromJson(Map<String, dynamic> json) => APIModel(
-      id: json['id'] as String,
-      url: json['url'] as String,
-      method: $enumDecode(_$HttpMethodEnumMap, json['method']),
-      headers: json['headers'] as Map<String, dynamic>?,
-      body: json['body'] as Map<String, dynamic>?,
-      bodyType: $enumDecodeNullable(_$BodyTypeEnumMap, json['bodyType']),
-      variables:
-          _$JsonConverterFromJson<Map<String, Object?>, Map<String, Variable>>(
-              json['variables'], const VariableJsonConverter().fromJson),
-    );
+  id: json['id'] as String,
+  url: json['url'] as String,
+  method: $enumDecode(_$HttpMethodEnumMap, json['method']),
+  headers: json['headers'] as Map<String, dynamic>?,
+  body: json['body'] as Map<String, dynamic>?,
+  bodyType: $enumDecodeNullable(_$BodyTypeEnumMap, json['bodyType']),
+  variables:
+      _$JsonConverterFromJson<Map<String, Object?>, Map<String, Variable>>(
+        json['variables'],
+        const VariableJsonConverter().fromJson,
+      ),
+);
 
 Map<String, dynamic> _$APIModelToJson(APIModel instance) => <String, dynamic>{
-      'id': instance.id,
-      'url': instance.url,
-      'method': _$HttpMethodEnumMap[instance.method]!,
-      'headers': instance.headers,
-      'body': instance.body,
-      'bodyType': _$BodyTypeEnumMap[instance.bodyType],
-      'variables':
-          _$JsonConverterToJson<Map<String, Object?>, Map<String, Variable>>(
-              instance.variables, const VariableJsonConverter().toJson),
-    };
+  'id': instance.id,
+  'url': instance.url,
+  'method': _$HttpMethodEnumMap[instance.method]!,
+  'headers': instance.headers,
+  'body': instance.body,
+  'bodyType': _$BodyTypeEnumMap[instance.bodyType],
+  'variables':
+      _$JsonConverterToJson<Map<String, Object?>, Map<String, Variable>>(
+        instance.variables,
+        const VariableJsonConverter().toJson,
+      ),
+};
 
 const _$HttpMethodEnumMap = {
   HttpMethod.get: 'GET',
@@ -42,16 +46,15 @@ const _$BodyTypeEnumMap = {
   BodyType.json: 'JSON',
   BodyType.multipart: 'MULTIPART',
   BodyType.formUrlEncoded: 'FORM_URLENCODED',
+  BodyType.graphql: 'GRAPHQL',
 };
 
 Value? _$JsonConverterFromJson<Json, Value>(
   Object? json,
   Value? Function(Json json) fromJson,
-) =>
-    json == null ? null : fromJson(json as Json);
+) => json == null ? null : fromJson(json as Json);
 
 Json? _$JsonConverterToJson<Json, Value>(
   Value? value,
   Json? Function(Value value) toJson,
-) =>
-    value == null ? null : toJson(value);
+) => value == null ? null : toJson(value);
