2c88e8ba09056f920c6923452dbd11dafabe4c77
Aditya <choubeyaditya80@gmail.com>
2025-09-27 00:20:35 +0530

    refactor: improve readability and remove unnecessary code

--- lib/src/framework/utils/flutter_extensions.dart
+++ lib/src/framework/utils/flutter_extensions.dart
@@ -34,10 +34,6 @@ extension ExtentUtil on String {
     final trimmedValue = trim();
     if (trimmedValue.isEmpty) return null;
 
-    if (trimmedValue.toLowerCase() == 'intrinsic') {
-      return null;
-    }
-
     if (trimmedValue.endsWith('%')) {
       final mediaQuerySize = MediaQuery.maybeSizeOf(context);
       if (mediaQuerySize == null) return null;
