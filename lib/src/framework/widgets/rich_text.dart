59299b4f7de7240388ec016a7be58285255f4af9
ShashwatXD <legendshashwat.gkp@gmail.com>
2025-12-16 17:57:12 +0530

    format

--- lib/src/framework/widgets/rich_text.dart
+++ lib/src/framework/widgets/rich_text.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
 import '../actions/base/action_flow.dart';
 import '../base/virtual_leaf_stateless_widget.dart';
 import '../models/types.dart';
