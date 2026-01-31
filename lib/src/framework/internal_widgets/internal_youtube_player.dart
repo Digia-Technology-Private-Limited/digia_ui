ab2790f650f757d7094307c22e129de00baf908d
RamSuthar-Digia <ramsuthar@digia.tech>
2026-01-31 14:47:14 +0530

    feat: integrate youtube_player_iframe for iframe support in InternalYoutubePlayer

--- lib/src/framework/internal_widgets/internal_youtube_player.dart
+++ lib/src/framework/internal_widgets/internal_youtube_player.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/widgets.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart'
+    as yt_flutter;
+import 'package:youtube_player_iframe/youtube_player_iframe.dart' as yt_iframe;
+
+import '../../dui_dev_config.dart';
+import '../../init/digia_ui_manager.dart';
 
 class InternalYoutubePlayer extends StatefulWidget {
   final String videoUrl;
@@ -20,25 +25,41 @@ class InternalYoutubePlayer extends StatefulWidget {
 }
 
 class _InternalYoutubePlayerState extends State<InternalYoutubePlayer> {
-  late YoutubePlayerController _controller;
+  late dynamic _controller;
+  bool _isIframe = false;
 
   @override
   void initState() {
     super.initState();
+    _isIframe = DigiaUIManager().host is DashboardHost;
     _initializeController();
   }
 
   void _initializeController() {
-    final String? videoId = YoutubePlayer.convertUrlToId(widget.videoUrl);
+    final String? videoId =
+        yt_flutter.YoutubePlayer.convertUrlToId(widget.videoUrl);
 
-    _controller = YoutubePlayerController(
-      initialVideoId: videoId ?? '',
-      flags: YoutubePlayerFlags(
+    if (_isIframe) {
+      _controller = yt_iframe.YoutubePlayerController.fromVideoId(
+        videoId: videoId ?? '',
         autoPlay: widget.autoPlay,
-        mute: widget.isMuted,
-        loop: widget.loop,
-      ),
-    );
+        params: yt_iframe.YoutubePlayerParams(
+          mute: widget.isMuted,
+          showControls: true,
+          showFullscreenButton: true,
+          loop: widget.loop,
+        ),
+      );
+    } else {
+      _controller = yt_flutter.YoutubePlayerController(
+        initialVideoId: videoId ?? '',
+        flags: yt_flutter.YoutubePlayerFlags(
+          autoPlay: widget.autoPlay,
+          mute: widget.isMuted,
+          loop: widget.loop,
+        ),
+      );
+    }
   }
 
   @override
@@ -51,18 +72,30 @@ class _InternalYoutubePlayerState extends State<InternalYoutubePlayer> {
   void didUpdateWidget(covariant InternalYoutubePlayer oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (widget.videoUrl != oldWidget.videoUrl) {
-      final String? videoId = YoutubePlayer.convertUrlToId(widget.videoUrl);
+      final String? videoId =
+          yt_flutter.YoutubePlayer.convertUrlToId(widget.videoUrl);
       if (videoId != null) {
-        _controller.load(videoId);
+        if (_isIframe) {
+          _controller.loadVideoById(videoId: videoId);
+        } else {
+          _controller.load(videoId);
+        }
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return YoutubePlayer(
-      controller: _controller,
-      showVideoProgressIndicator: true,
-    );
+    if (_isIframe) {
+      return yt_iframe.YoutubePlayer(
+        controller: _controller,
+        aspectRatio: 16 / 9,
+      );
+    } else {
+      return yt_flutter.YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
+      );
+    }
   }
 }
